18d5e20d402322e07f51b0b08529e52f278de266
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-23 18:52:54 +0600

    local host origin changed

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const http = require('http');
 const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-    },
-  });
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
