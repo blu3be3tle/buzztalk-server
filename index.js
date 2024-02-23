5a8b51910a5a9f3756986669b697a64dc7791a18
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-23 18:48:59 +0600

    basic server setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-    
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const { Server } = require('socket.io');
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
