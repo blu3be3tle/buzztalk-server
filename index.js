1a52377c54f1e09b41b47bc226b58199dfc36696
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-24 14:07:17 +0600

    server listen

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require('express');
 const app = express();
-
 const http = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
+app.use(cors());
+
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
+});
+
+server.listen(3000, () => {
+  console.log('SERVER RUNNING');
 });
