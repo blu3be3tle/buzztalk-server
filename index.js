0b67bb5f120361848dc7ea1dd60720c337088dac
azzizulhakim810 <azzizul.hakim202@gmail.com>
2024-02-24 17:26:51 +0600

    setting up the server from my side

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const http = require('http');
-const { Server } = require('socket.io');
+const http = require("http");
+const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
   },
 });
+
+// I'm Working on
