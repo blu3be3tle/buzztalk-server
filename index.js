105f54e84fe0c408050ddbe272f85132b5da41eb
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-25 12:28:20 +0600

    disconnect event integrated

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 
 });
 
