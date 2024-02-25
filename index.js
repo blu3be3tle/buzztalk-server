8bc1af6938120e33c39e6b6916f3a6d03b995992
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-26 02:28:54 +0600

    localhost origin bug fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5',
     methods: ['GET', 'POST'],
   },
 });
@@ -23,8 +23,8 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on('send_message', (msg) => {
-    console.log('message: ' + msg);
+  socket.on('send_message', (data) => {
+    console.log(data);
   });
 
   socket.on('disconnect', () => {
