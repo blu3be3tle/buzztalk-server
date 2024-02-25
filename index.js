2a5db5fadd082733273473ff0bb36d0c9fd576e4
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-26 02:03:33 +0600

    chat message event

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
