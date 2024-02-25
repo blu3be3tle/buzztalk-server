f1ae9c049fb4bff7703ccde0c69ea9613f5ed951
Rezwan66 <shaikhrezwan66@gmail.com>
2024-02-25 15:12:38 +0100

    set up socket server and io method

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+const express = require("express");
 const app = express();
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
 });
