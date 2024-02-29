d970c4757c24b140ddd704a66022d8f7633f1fe5
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-03-01 00:27:16 +0600

    server listen bug fixed

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const cors = require('cors');
 const { Server } = require('socket.io');
 app.use(cors());
 
-const io = require('socket.io')(process.env.PORT || 3000, {
+const server = http.createServer(app);
+
+const io = new Server(server, {
   cors: {
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
@@ -28,4 +30,8 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+});
+
+server.listen(3000, () => {
+  console.log('SERVER RUNNING');
 });
